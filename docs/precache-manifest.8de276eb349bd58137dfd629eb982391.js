39f7d177c04c665a1b296385c7a3d20cdfd03537
facudelucia <facundodeluciajudo@gmail.com>
2020-08-03 02:32:15 -0300

    docs agregado

---
+++ docs/precache-manifest.8de276eb349bd58137dfd629eb982391.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd3d9a8ae29a02db0486f5c98784ced",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e7ad6bea748c9dc7492",
+    "url": "/static/css/main.6d9180e2.chunk.css"
+  },
+  {
+    "revision": "bf7e541b29b38394ba5e",
+    "url": "/static/js/2.c85fab59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c85fab59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7ad6bea748c9dc7492",
+    "url": "/static/js/main.086bfc03.chunk.js"
+  },
+  {
+    "revision": "dab11903091cda04344c",
+    "url": "/static/js/runtime-main.71f22666.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/static/media/logo.427a4adc.svg"
+  }
+]);
